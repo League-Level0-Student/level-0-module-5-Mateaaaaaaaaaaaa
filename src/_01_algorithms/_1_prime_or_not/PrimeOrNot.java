34ada194e5981e3b10f1b61f78be0c43b4e41da1
student <student@192.168.1.54>
2023-09-14 14:03:13 -0700

    prime or not

---
+++ src/_01_algorithms/_1_prime_or_not/PrimeOrNot.java
@@ -0,0 +1,22 @@
+package _01_algorithms._1_prime_or_not;
+
+import javax.swing.JOptionPane;
+
+public class PrimeOrNot {
+	public static void main(String[] args) {
+		String choose=JOptionPane.showInputDialog("choose a number");
+		int number= Integer.parseInt(choose);
+		for (int i =2;i< number;i++) {
+			if(number%i==0){
+				System.out.println("is not PRIME ");
+			JOptionPane.showMessageDialog(null, number+"is not PRIME ");
+			System.exit(0);
+			}
+		}
+	}
+
+
+
+
+
+}
