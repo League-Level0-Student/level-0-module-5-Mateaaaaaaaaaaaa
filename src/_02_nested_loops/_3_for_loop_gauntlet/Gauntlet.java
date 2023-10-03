67f1b0da779b18a0cddd53406b2a6be68395c587
student <student@192.168.1.25>
2023-10-03 14:02:15 -0700

    progress

---
+++ src/_02_nested_loops/_3_for_loop_gauntlet/Gauntlet.java
@@ -0,0 +1,42 @@
+package _02_nested_loops._3_for_loop_gauntlet;
+
+public class Gauntlet {
+	public static void main(String[] args) {
+
+		for(int i = 0 ; i<=100; i++ ) {
+			///System.out.println(i);
+
+		}
+
+		for(int i = 100 ; i>=0; i--) {
+			//System.out.println(i);
+		}
+
+
+		for(int i = 2 ; i<=100; i++ ) {
+
+			if(i%2==0) {
+				//	System.out.println(i);
+			}
+		}
+		for (int i =0; i<=100; i+=2) {
+			//System.out.println(i);
+
+		}
+		for(int i = 1 ; i<=99; i+=2) {
+			///	System.out.println(i);
+
+		}
+
+		for(int i = 1 ; i<=500; i++) {
+			if (i%2==0) {
+				System.out.println(i+" is even");
+			}
+			if (i%2==1) {
+				System.out.println(i +" is odd");
+			}
+		}
+	}
+
+
+}
--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -14,19 +14,35 @@ public class HappyPet {
 		String petType=JOptionPane.showInputDialog("what kind of pet do you want to buy ?");
 
 		// 3. REPEAT steps 4 - 7 enough times to make your pet happy!
+		while (true) {
+			// 4. Use showOptionDialog to ask the user what they want to do to make their pet happy
+			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
+			//    Make sure to customize the title and question too.
+			int task = JOptionPane.showOptionDialog(null, "what do you want to do with your pet ? Your pet hapiness is currently "+ happinessLevel+ "%", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "feed", "play", "walk" }, null);
 
-		// 4. Use showOptionDialog to ask the user what they want to do to make their pet happy
-		//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
-		//    Make sure to customize the title and question too.
-		int task = JOptionPane.showOptionDialog(null, "what do you want to do with your pet ? ", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-				new String[] { "feed", "play", "walk" }, null);
+			// 6. Use user input to call the appropriate method created in step 5 below.
+			System.out.println(task);
 
-		// 6. Use user input to call the appropriate method created in step 5 below.
+			if (task==0) {
+				feed(petType);
+			}
+			if (task==1) {
+				play(petType);
 
-		// 7. If you determine the happiness level is large enough, tell the
-		//    user that he loves his pet and use break; to exit for loop.
-
-	}
+			}
+			if (task==2) {
+				walk(petType);
+			}
+			// 7. If you determine the happiness level is large enough, tell the
+			//    user that he loves his pet and use break; to exit for loop.
+			if (happinessLevel==50) {
+				JOptionPane.showMessageDialog(null,"you love your "+petType);
+				
+				break;
+			}
+		}
+	}	
 
 	// 5. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
@@ -46,15 +62,33 @@ public class HappyPet {
 		happinessLevel +=10;
 	}
 
-	static void play() {
+	static void play(String pet) {
 
-
-
+		if(pet.equals("dog")) {
+			JOptionPane.showMessageDialog(null,"Your dog wants to play");
+		}
+		if(pet.equals("horse")) {
+			JOptionPane.showMessageDialog(null,"Your horse wants to play");
+		}
+		if(pet.equals("fish")) {
+			JOptionPane.showMessageDialog(null,"Your fish wants to plays");
+		}
+		happinessLevel +=10;
 	}
 
-	static void walk() {
 
+	static void walk(String pet) {
 
-
+		if(pet.equals("dog")) {
+			JOptionPane.showMessageDialog(null,"Your dog is agited");
+		}
+		if(pet.equals("horse")) {
+			JOptionPane.showMessageDialog(null,"Your horse is jumping in the field");
+		}
+		if(pet.equals("fish")) {
+			JOptionPane.showMessageDialog(null,"Your fish is bored ");
+		}
+		happinessLevel +=10;
 	}
 }
+
