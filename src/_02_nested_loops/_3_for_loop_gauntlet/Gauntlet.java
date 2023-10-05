4f57de38adc4e3743c25c70a7b8d5349420f5aaa
student <student@192.168.1.25>
2023-10-05 13:51:40 -0700

    progress

--- src/_02_nested_loops/_3_for_loop_gauntlet/Gauntlet.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/Gauntlet.java
@@ -30,13 +30,54 @@ public class Gauntlet {
 
 		for(int i = 1 ; i<=500; i++) {
 			if (i%2==0) {
-				System.out.println(i+" is even");
+				//		System.out.println(i+" is even");
 			}
 			if (i%2==1) {
-				System.out.println(i +" is odd");
+				//		System.out.println(i +" is odd");
+
 			}
+
+
+		}
+
+		//step 6 
+		for(int i =0 ; i<777; i+= 7) {
+			if (i%7==0) {
+				//	System.out.println(i+"is a multiple");
+
+			}
+
+
+		}
+
+
+		//step 7
+		for (int i=0; i<17; i++) {
+			//System.out.println(" I was "+i+" in "+(2007+i));
+
+
+
+
+		}
+
+		//step 1 for-loops
+
+		for (int i=0; i<3; i++){
+			for(int j=0; j<3; j++) {
+				//	System.out.println(i+""+j);
+
+
+			}
+
+		}
+
+
+		for (int i=1; i<4; i++) {
+			for(int j=1; j<4; j++) {
+				System.out.println();
+
+			}
+
 		}
 	}
-
-
 }
